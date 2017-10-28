b2a8897c5d86c6914d3b269f13463be89d918684
Sacha IFRAH <ifrah.sacha@gmail.com>
2017-10-28 02:37:42 +0200

    first version of perfect bandwidth

--- index.js
+++ index.js
@@ -8,29 +8,31 @@ class PerfectBandwidth {
       var network = require('network');
       var devicesFound = 1;
       var self = this;
-
       network.get_active_interface(function(err, obj) {
-        const discovery = require('device-discovery')({ type: 'ICMP', iface: obj.name })
+        if (err != null) {
+          self.emit('perfectBandwidth', defaultBandwidth)
+        } else {
+          const discovery = require('device-discovery')({ type: 'ICMP', iface: obj.name })
 
-        discovery.on('device', function(){
-          devicesFound += 1
-        })
+          discovery.on('device', function(){
+            devicesFound += 1
+          })
 
-        discovery.on('done', function() {
-          let speedTest = require('speedtest-net');
-          let test = speedTest({maxTime: 15000});
+          discovery.on('done', function() {
+            let speedTest = require('speedtest-net');
+            let test = speedTest({maxTime: 15000});
 
-          test.on('data', data => {
-            let bytesPerSeconds = bytes(data.speeds.upload+'MB');
-            let shareBandwidth = bytesPerSeconds/devicesFound;
-            self.emit('perfectBandwidth', shareBandwidth/4)
-          });
+            test.on('data', data => {
+              let bytesPerSeconds = bytes(data.speeds.upload+'MB');
+              let shareBandwidth = bytesPerSeconds/devicesFound;
+              self.emit('perfectBandwidth', shareBandwidth/4)
+            });
 
-          test.on('error', err => {
-            self.emit('perfectBandwidth', defaultBandwidth)
-          });
-
-        })
+            test.on('error', err => {
+              self.emit('perfectBandwidth', defaultBandwidth)
+            });
+          })
+        }
       })
     }
 }

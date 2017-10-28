06b4a65bc74f339ec1f6bc37be94839c9801c232
Sacha IFRAH <ifrah.sacha@gmail.com>
2017-10-28 02:00:42 +0200

    first version of perfect bandwidth

--- index.js
+++ index.js
@@ -1,36 +1,34 @@
 'use strict';
 
 var events        = require('events');
+var bytes         = require('bytes');
 
 class PerfectBandwidth {
-    constructor(){
-      /*let speedTest = require('speedtest-net');
-      let test = speedTest({maxTime: 5000});
-
-      test.on('data', data => {
-        console.dir(data);
-      });
-
-      test.on('error', err => {
-        console.error(err);
-      });*/
+    constructor(defaultBandwidth = 80000){
       var network = require('network');
+      var devicesFound = 0;
+      var self = this;
+
       network.get_active_interface(function(err, obj) {
         const discovery = require('device-discovery')({ type: 'ICMP', iface: obj.name })
-
-        discovery.on('device', console.log)
-
+        discovery.on('device', function(){
+          devicesFound += 1
+        })
         discovery.on('done', function() {
+
           let speedTest = require('speedtest-net');
-          let test = speedTest({maxTime: 5000});
+          let test = speedTest({maxTime: 15000});
 
           test.on('data', data => {
-            console.dir(data);
+            let bytesPerSeconds = bytes(data.speeds.upload+'MB');
+            let shareBandwidth = bytesPerSeconds/devicesFound;
+            self.emit('perfectBandwidth', shareBandwidth/4)
           });
 
           test.on('error', err => {
-            console.error(err);
+            self.emit('perfectBandwidth', defaultBandwidth)
           });
+
         })
       })
     }
--- tests/index.js
+++ tests/index.js
@@ -1,3 +1,6 @@
 var PerfectBandwidth = require('../index.js')
 
 var pb  =   new PerfectBandwidth();
+pb.on('perfectBandwidth', function(debitMaximum) {
+  console.log("Maximum debit", ((debitMaximum|0)/1024)|0, "Kb/s")
+});
